2e475a0124fb0277d7bcdcff93c7f08e538293c0
Leonardo Jurado <leonardo.jurado@decameron.com>
2019-04-22 22:12:29 -0500

    CrossDomain

--- login.php
+++ login.php
@@ -4,6 +4,8 @@ header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Access-Control-Allow-Credentials: true');
+
 
 $response = new stdClass;
 $response->mensaje = "Hola leo, has iniciado sesion con exito!";
