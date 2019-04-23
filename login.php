4857b11fe9753e74aa7a589717cf73a6b934222d
Leonardo Jurado <leonardo.jurado@decameron.com>
2019-04-22 22:10:19 -0500

    CrossDomain

--- login.php
+++ login.php
@@ -1,5 +1,10 @@
 <?php
 
+header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
 $response = new stdClass;
 $response->mensaje = "Hola leo, has iniciado sesion con exito!";
 $response->user->password = null;
@@ -15,6 +20,7 @@ $response->user->accountNonExpired = true;
 $response->user->accountNonLocked = true;
 $response->user->credentialsNonExpired = true;
 $response->user->enabled = true;
+$response->server = $_SERVER['HTTP_ORIGIN'];
 $response->token = "eyJhbGciOiJIUzUxMiJ9.eyJhdXRob3JpdGllcyI6Ilt7XCJhdXRob3JpdHlcIjpcIlJPTEVfQ0xJRU5URVwifV0iLCJzdWIiOiJsZWlkeSIsImlhdCI6MTU1NTcxMTAzNCwiZXhwIjoxNTU1NzI1MDM0fQ.9QchcrsrwF7f_Ihy6Kb4tzJpayV0ZzbuDKaQwUT5N0rShLVnEEWvz6aQvNm-gmMcCy7ePpnJertnO_8iNbuwBQ";
 
 //RESPUESTA
